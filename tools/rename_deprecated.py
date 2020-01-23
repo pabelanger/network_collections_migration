2cd60f26274fa0d5a7409a62be29a0be2ef4b5de
Paul Belanger <pabelanger@redhat.com>
2020-01-23 15:27:03 -0500

    Add base scenario example
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- tools/rename_deprecated.py
+++ tools/rename_deprecated.py
@@ -5,6 +5,9 @@ for dirpath, dirname, filenames in os.walk('.'):
     for f in filenames:
         if '__init__.py' in f or not f.startswith('_'):
             continue
+        # NOTE(pabelanger): _net_* renames
+        if '_net_' not in f:
+          continue
         src = os.path.join(dirpath, f)
         dest = os.path.join(dirpath, f[1:])
         print('Renaming %s -> %s', src, dest)
